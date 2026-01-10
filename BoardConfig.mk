a4e24556a9f57d7b279707a88e3dc392d7d9742e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-01-10 07:54:33 +0100

    chenfeng: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,8 +73,8 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/hidl/compatibility_matrix.device.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml

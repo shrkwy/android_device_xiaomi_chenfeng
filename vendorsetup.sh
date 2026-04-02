277c58ae44d910e598d7aa9ff6a5f79aa74d281c
SMGReborn <vherawidatama@gmail.com>
2026-04-02 16:31:03 +0200

    chenfeng: add vendorsetup to clone stuff automatically
    Signed-off-by: SMGReborn <vherawidatama@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Welcome to SMGReborn Teams
+# This script for auto cloning some stuff before building
+
+# Clone kernel tree
+git clone https://github.com/Little-Chenfeng-ID/device_xiaomi_chenfeng-kernel.git -b lineage-23.2 device/xiaomi/chenfeng-kernel
+
+# Clone hardware xiaomi
+git clone https://github.com/Little-Chenfeng-ID/hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+
+# Clone vendor device chenfeng
+git clone https://codeberg.org/smgreborn/vendor_xiaomi_chenfeng.git -b lineage-23.2 vendor/xiaomi/chenfeng
+
+# Finish clone all stuff
+# Happy Build and Brick

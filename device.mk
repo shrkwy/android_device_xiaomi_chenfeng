062ce7a66e5a8c85bae0bac1e1054a6ae7e826f8
SMGReborn <vherawidatama@gmail.com>
2026-05-17 15:14:37 +0000

    chenfeng: fixup IR Blaster
    Signed-off-by: SMGReborn <vherawidatama@gmail.com>

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.lineage
+    android.hardware.ir-service.example
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
